bafd6f47e54b87a0e0037b968f153164a0b3ab3a
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-01-07 16:12:31 +0100

    class-functional component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import Count from './functionalComponent'
+import ExampleClassComponen from './classcomponent'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>changing react class component to functional component</h2>
+        <ExampleClassComponen/>
+        <Count /><br />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/classcomponent.jsx
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+
+class ExampleClassComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`;
+  }
+
+  componentDidUpdate() {
+    document.title = `Clicked ${this.state.count} times`;
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked class component {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ExampleClassComponent;
